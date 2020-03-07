5073bf6b8f43ae8153baad470ddc68889baf448d
crabcyborg <mike@crabcyb.org>
2020-03-07 15:04:25 -0400

    update homepage text

--- containers/Home.js
+++ containers/Home.js
@@ -7,9 +7,8 @@ export var Home = {
 	view: v => [
 		m(Logo),
 		m('h3', "Welcome to Crab Cyborg!"),
-		m('p', 'This entire website is ', m('a', { href: github, target: '_blank' }, 'open source'), ' and built on ', m('a', { href: 'https://mithril.js.org/' }, 'mithril.js.')),
+		m('p', 'This entire website is ', m('a', { href: github, target: '_blank' }, 'open source'), ' and built on ', m('a', { href: 'https://mithril.js.org/' }, 'mithril.js'), '. I have also begun ', m('a', {href: 'https://flutter.crabcyb.org'}, ' experimenting with Flutter'), '.'),
 		m('p', "This is an experimental blog/tutorial series where we start by putting together our very own public domain Frankenstein."),
-		m('p', "I have also begun ", m('a', {href: 'https://flutter.crabcyb.org'}, 'an experimental series where I use Flutter with Dart'), "."),
 		m('h4.mb0', 'Here are the posts:'),
 		m(Posts),
 		m('h4.mb0', 'Here are the experiments:'),
